7a1d797371ae2fbb7ab4ab3a17d1aad99224a333
MahouOcty <mahouocty@gmail.com>
2024-05-31 11:48:04 -0500

    API Reciever

--- Assets/Scripts/ClosePanel.cs
+++ Assets/Scripts/ClosePanel.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class ClosePanel : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject apiReciever;
     public Text buttonText;
 
     public void SetActive()
     {
         if (panel.activeSelf){
             panel.SetActive(false);
+            apiReciever.SetActive(false);
             buttonText.text = "Open";
         }
         else { 
             panel.SetActive(true);
+            apiReciever.SetActive(true);
             buttonText.text = "Close";
         }
     }
